54e02f58f7a698fed4e7dd7b327f13dab3084df4
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-03-02 11:21:01 +0700

    ShopTest Update

--- src/test/java/model/ShopTest.java
+++ src/test/java/model/ShopTest.java
@@ -1,20 +1,19 @@
 package model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import model.Item;
+import model.Shop;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopTest {
 
     @Test
+    @DisplayName("Add Item into Shop")
     void addItem() {
-    }
-
-    @Test
-    void getAllItems() {
-    }
-
-    @Test
-    void getAllItemSold() {
+        Item barang_1 = new Item("Sabun Mandi", 20, 100);
+        Shop myShop = new Shop();
+        assertEquals("",myShop.addItem(new Item(barang_1));
     }
 }
