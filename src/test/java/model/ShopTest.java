92fcae4654ea32d322a7b852c45b0a616d422105
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-03-02 12:33:47 +0700

    update shopTest

--- src/test/java/model/ShopTest.java
+++ src/test/java/model/ShopTest.java
@@ -7,13 +7,17 @@ import model.Shop;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class ShopTest {
 
     @Test
     @DisplayName("Add Item into Shop")
     void getItems() {
-        Item barang_1 = new Item("Sabun Mandi", 20, 100);
+        Item barang1 = new Item("Sabun Mandi", 20, 100);
         Shop myShop = new Shop();
-        assertEquals("",myShop.getItems());
+        myShop.addItem(barang1);
+        String result = "Item name: 'Sabun Mandi', Point: 20, Stock Available: 100, Item Sold:  0";
+        assertEquals(result,myShop.getItems());
     }
 }
