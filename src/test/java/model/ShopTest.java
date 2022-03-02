9bb9a3f5cbe71575935470b647a50f3ef3b95eda
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-03-02 11:12:38 +0700

    add test

---
+++ src/test/java/model/ShopTest.java
@@ -0,0 +1,20 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopTest {
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void getAllItems() {
+    }
+
+    @Test
+    void getAllItemSold() {
+    }
+}
